f6ad23870caffe553b4e075283fa7d60066129c9
KvotheSixStrings <ben0g@yahoo.com>
2016-05-02 22:32:02 -0500

    adds scale to dirTrans

--- Components/DirectionalTransform.cs
+++ Components/DirectionalTransform.cs
@@ -4,32 +4,32 @@ using System.Collections;
 public class DirectionalTransform : DirectionalComponent {
 
 	public Vector3 leftPosition;
-    public Vector3 leftScale = Vector3.one;
+    
     public Vector3 rightPosition;
-    public Vector3 rightScale = Vector3.one;
+    
     public Vector3 upPosition;
-    public Vector3 upScale = Vector3.one;
+    
     public Vector3 downPosition;
-    public Vector3 downScale = Vector3.one;
+    
 
 	protected override void SetLeft () {
 		transform.localPosition = leftPosition;
-        transform.localScale = leftScale;
+       
 	}
 
 	protected override void SetRight () {
 		transform.localPosition = rightPosition;
-        transform.localScale = rightScale;
+       
 	}
 
 	protected override void SetUp () {
 		transform.localPosition = upPosition;
-        transform.localScale = upScale;
+        
 	}
 
 	protected override void SetDown () {
 		transform.localPosition = downPosition;
-        transform.localScale = downScale;
+        
 	}
 
 	void OnDrawGizmos () {
